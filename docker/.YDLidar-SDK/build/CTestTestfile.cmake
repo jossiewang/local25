b388f8c7f48dfff3ee2c4308d12e05c060f97921
jossiewang <jossiew621@gapp.nthu.edu.tw>
2024-07-31 10:39:08 +0800

    Feat: Add LiDAR dependencies
    Add:
        YDLidar-SDK dependency folder
        building procedure in Dockerfile

---
+++ docker/.YDLidar-SDK/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/localization/localization_ws/src/Eurobot-2024-Localization/YDLidar-SDK
+# Build directory: /home/localization/localization_ws/src/Eurobot-2024-Localization/YDLidar-SDK/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("core")
+subdirs("src")
+subdirs("samples")
+subdirs("test")
---
+++ docker/.YDLidar-SDK/build/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/localization/localization_ws/src/Eurobot-2024-Localization/YDLidar-SDK/test
+# Build directory: /home/localization/localization_ws/src/Eurobot-2024-Localization/YDLidar-SDK/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(lidar_test "lidar_test")
+set_tests_properties(lidar_test PROPERTIES  _BACKTRACE_TRIPLES "/home/localization/localization_ws/src/Eurobot-2024-Localization/YDLidar-SDK/test/CMakeLists.txt;10;add_test;/home/localization/localization_ws/src/Eurobot-2024-Localization/YDLidar-SDK/test/CMakeLists.txt;0;")
